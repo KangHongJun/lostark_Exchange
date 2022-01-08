ff080b56e1342eda129c71b1e9a5fc9d9996249e
KangHongJun <40516203+KangHongJun@users.noreply.github.com>
2022-01-08 21:41:50 +0900

    set data in tablewidget
    set data

--- Function.py
+++ Function.py
@@ -1,4 +1,19 @@
-import re
+import sqlite3
+import pandas as pd
+
+#db데이터 읽어오기 - DB파일로 옮기거나 하기
+def Reforging_Data():
+    conn = sqlite3.connect("Reinforce.db")
+    Reforging = pd.read_sql("SELECT * FROM Reforging",conn,index_col=None)
+    Reforging_List = Reforging.values.tolist()
+    return Reforging_List
+
+def Reforging_Add_Data():
+    conn = sqlite3.connect("Reinforce.db")
+    Reforging_Add = pd.read_sql("SELECT * FROM Reforging_Add",conn,index_col=None)
+    Reforging_Add_List = Reforging_Add.values.tolist()
+    return Reforging_Add_List
+
 
 
 def check(value):
--- UI/textUI.py
+++ UI/textUI.py
@@ -1,9 +1,27 @@
 from PyQt5.QtWidgets import *
+import Function
+
+Reforging = Function.Reforging_Data()
+Reforging_Add = Function.Reforging_Add_Data()
+
+def SetTable(self,list):
+    i = 0
+    for col, row in list:
+        row = str(row)
+        name = QTableWidgetItem(col)
+        price = QTableWidgetItem(row)
+        self.tableWidget.setItem(i, 0, name)
+        self.tableWidget.setItem(i, 1, price)
+        i = i + 1
 
 def low0(self):
     self.tableWidget = QTableWidget(self)
     self.tableWidget.setRowCount(53)
     self.tableWidget.setColumnCount(2)
+    column_headers = ['아이템명', '가격']
+    self.tableWidget.setHorizontalHeaderLabels(column_headers)
+
+    SetTable(self,Reforging + Reforging_Add)
 
     layout = QFormLayout()
     label1 = QLabel()
@@ -14,8 +32,12 @@ def low0(self):
 
 def low1(self):
     self.tableWidget = QTableWidget(self)
-    self.tableWidget.setRowCount(21)
     self.tableWidget.setColumnCount(2)
+    self.tableWidget.setRowCount(21)
+    column_headers = ['아이템명','가격']
+    self.tableWidget.setHorizontalHeaderLabels(column_headers)
+
+    SetTable(self,Reforging)
 
     layout = QFormLayout()
     label1 = QLabel()
@@ -28,6 +50,10 @@ def low2(self):
     self.tableWidget = QTableWidget(self)
     self.tableWidget.setRowCount(32)
     self.tableWidget.setColumnCount(2)
+    column_headers = ['아이템명', '가격']
+    self.tableWidget.setHorizontalHeaderLabels(column_headers)
+
+    SetTable(self,Reforging_Add)
 
     layout = QFormLayout()
     label1 = QLabel()
